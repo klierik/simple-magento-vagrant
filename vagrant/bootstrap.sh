774c517c7069cd1aa05a2964cecac15d3745acfe
Christoph Klocker <christoph@vedanova.com>
2015-07-24 12:36:35 +0200

    switched to normal vbox mount, enabled phpcrypt modul on setup

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -12,6 +12,8 @@ apt-get install -y php5
 apt-get install -y libapache2-mod-php5
 apt-get install -y php5-mysql php5-curl php5-gd php5-intl php-pear php5-imap php5-mcrypt php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php-apc
 
+#enable mcrypt
+php5enmod mcrypt
 
 # Delete default apache web dir and symlink mounted vagrant dir from host machine
 # --------------------
